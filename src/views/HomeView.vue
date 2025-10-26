36bc42feba04c0f1c2fe672abeadb5442768a52e
Massimiliano Prandi <maxallep@gmail.com>
2025-10-26 18:01:55 +0100

    Aggiunto messaggio di errore per dispositivi non mobile o con schermi troppo grandi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
 import ButtonHome from '@/components/ButtonHome.vue'
+import { ref, onMounted } from 'vue'
 import { goToPage } from '@/router/navigation'
+
+const isMobile = ref(true)
+
+onMounted(() => {
+  const userAgent = navigator.userAgent
+  const isMobileDevice = /android|iphone|ipad|ipod|windows phone/i.test(userAgent.toLowerCase())
+  const isScreenSmall = window.innerWidth <= 768
+  isMobile.value = isMobileDevice && isScreenSmall
+})
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center h-screen w-screen bg-neutral-800">
+  <div v-if="!isMobile" class="flex justify-center items-center h-screen w-screen bg-neutral-800 text-center px-4">
+    <p class="text-red-700 font-extrabold text-2xl">
+      L'accesso al gioco non è disponibile da computer. L'app e' pensata esclusivamente per dispositivi mobili.
+    </p>
+  </div>
+  <div v-else class="flex flex-col justify-center items-center h-screen w-screen bg-neutral-800">
     <p class="text-red-700 font-extrabold text-6xl mb-20 -mt-[12vh]">IMPOSTORE</p>
     <div class="grid grid-cols-1 gap-y-6">
       <ButtonHome buttonText="Nuova Partita" @click="goToPage('newGame')"/>
